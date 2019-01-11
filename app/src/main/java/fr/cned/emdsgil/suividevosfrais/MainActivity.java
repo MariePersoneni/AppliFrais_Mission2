2f45a1b03eddf3968bc000c90af1a632ff3f07e2
marie <mpersoneni@opium.eu>
2019-01-11 17:15:41 +0100

    oubli au niveau de icon_frais_repas => OK

--- app/src/main/java/fr/cned/emdsgil/suividevosfrais/MainActivity.java
+++ app/src/main/java/fr/cned/emdsgil/suividevosfrais/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         // chargement des méthodes événementielles
         cmdMenu_clic(((ImageButton) findViewById(R.id.cmdKm)), KmActivity.class);
         cmdMenu_clic(((ImageButton) findViewById(R.id.cmdHf)), HfActivity.class);
+        cmdMenu_clic(((ImageButton) findViewById(R.id.cmdRepas)), RepasActivity.class);
         cmdMenu_clic(((ImageButton) findViewById(R.id.cmdNuitee)), NuiteeActivity.class);
         cmdMenu_clic(((ImageButton) findViewById(R.id.cmdEtape)), EtapeActivity.class);
         cmdMenu_clic(((ImageButton) findViewById(R.id.cmdHfRecap)), HfRecapActivity.class);
